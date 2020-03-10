4a57d661042e1a791597df0845c73a349bb8d7db
Natalia Acevedo <51829472+Nataliaprins@users.noreply.github.com>
2020-03-09 19:46:29 -0500

    Final

--- 03-python=1/q06=1/question.py
+++ 03-python=1/q06=1/question.py
@@ -21,7 +21,18 @@
 ##  jjj,2,5
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
-
-
-
+datos=list(open('data.csv','r'))
+datos=[l.split('\t') for l in datos]
+col5=[l[4].replace('\n','') for l in datos]
+col5=[l.split(',') for l in col5]
+col5=sum(col5,[])
+col5=[l.split(':') for l in col5]
+col5
+letras=sorted(list(set([l[0] for l in col5])))
+for l in letras:
+    lista=[]
+    for k in col5:
+        if k[0]==l:
+            lista.append(int(k[1]))
+    print(l,min(lista),max(lista),sep=',')
+#en cada uno de los valores asociados de las clave hay ceros, por tanto, el mínimo de la lista en cada posisión es 0, en el test del código se expresan valores mayores
